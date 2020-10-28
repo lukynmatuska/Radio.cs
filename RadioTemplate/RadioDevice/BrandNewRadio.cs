23e63e70470854bb41e5562564d3be240ab77047
Lukáš Matuška <lukynmatuska@gmail.com>
2020-10-28 20:39:32 +0100

    BrandNewRadio

--- RadioTemplate/RadioDevice/BrandNewRadio.cs
+++ RadioTemplate/RadioDevice/BrandNewRadio.cs
@@ -8,30 +8,40 @@ namespace RadioTemplate.RadioDevice
     public class BrandNewRadio : IRadioDevice
     {
         private List<RadioMessage> messageHistory;
+        private RadioStation station;
 
         public BrandNewRadio(RadioStation radioStation)
         {
-            ;
+            this.station = radioStation;
         }
 
         public IEnumerable<RadioMessage> RadioSavedNews()
         {
-            throw new NotImplementedException();
+            return messageHistory.FindAll(ms => ms.MessageType == RadioMessageType.News);
         }
 
         public IEnumerable<RadioMessage> RadioSavedMusic()
         {
-            throw new NotImplementedException();
+            return messageHistory.FindAll(ms => ms.MessageType == RadioMessageType.Music);
         }
 
         public void TurnOffRadio()
         {
-            throw new NotImplementedException();
+            station.OnRadioBroadcast += Station_OnRadioBroadcast;
         }
 
         public void TurnOnRadio()
         {
-            throw new NotImplementedException();
+            station.OnRadioBroadcast -= Station_OnRadioBroadcast;
+        }
+
+        private void Station_OnRadioBroadcast(object sender, RadioMessage e)
+        {
+            if(e.MessageType == RadioMessageType.News || e.MessageType == RadioMessageType.Music)
+            {
+                messageHistory.Add(e);
+            }
+            Console.WriteLine($"[BrandNewRadio] [{e.MessageType}] {e.Moderator}: {e.Message}");
         }
     }
 }
